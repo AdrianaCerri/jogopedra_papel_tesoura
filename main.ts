c0ebb301f38f254e59913d9d22d78cff5337d2d1
AdrianaCerri <111378679+AdrianaCerri@users.noreply.github.com>
2022-09-05 14:20:03 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onGesture(Gesture.Shake, function () {
+    mão = randint(0, 2)
+    if (mão == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (mão == 1) {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+let mão = 0
+mão = 0
